e0e9b957480b92adbe486da6a3801bfac40532ea
pa-khan <don@pa-khan.com>
2025-07-07 00:28:24 +0300

    env

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,18 +5,27 @@ import { BrandsModule } from 'src/modules/brands/brands.module';
 import { CategoriesModule } from 'src/modules/categories/categories.module';
 import { ProductsModule } from 'src/modules/products/products.module';
 import { StocksModule } from 'src/modules/stocks/stocks.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres', // Тип базы данных
-      host: 'localhost', // или IP-адрес вашего сервера БД
-      port: 5432, // Стандартный порт Postgres
-      username: 'demo', // Имя пользователя БД
-      password: 'demo', // Пароль
-      database: 'm4t-catalog', // Название БД
-      autoLoadEntities: true,
-      synchronize: true, // ВАЖНО: true только для разработки! Автоматически создает/обновляет таблицы.
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      // @ts-ignore
+      useFactory: (configService: ConfigService) => ({
+        type: configService.get('DB_TYPE'),
+        host: configService.get('DB_HOST'),
+        port: Number(configService.get<number>('DB_PORT')),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: configService.get('NODE_ENV') !== 'production',
+      }),
+      inject: [ConfigService],
     }),
     TagsModule,
     BrandsModule,
